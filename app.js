b4f8c2f18c99362da07102666c6565f6f4eb0b37
shera <shera@sheras-MacBook-Pro.local>
2025-07-30 20:13:39 +0500

    Adding the URL Shortener using the Node.js

---
+++ app.js
@@ -0,0 +1,40 @@
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const Url = require("./models/Url");
+const { nanoid } = require("nanoid");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static("public"));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.error(err));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.post("/shorten", async (req, res) => {
+  const { fullUrl } = req.body;
+  const shortId = nanoid(6);
+  const shortUrl = `${req.protocol}://${req.get("host")}/${shortId}`;
+
+  await Url.create({ full: fullUrl, short: shortId });
+
+  res.render("result", { fullUrl, shortUrl });
+});
+
+app.get("/:shortId", async (req, res) => {
+  const { shortId } = req.params;
+  const entry = await Url.findOne({ short: shortId });
+
+  if (entry) return res.redirect(entry.full);
+  res.status(404).send("URL not found");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
